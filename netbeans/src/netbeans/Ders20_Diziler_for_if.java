aed466ebfa89fc5dd2b99d3c44e08d1f5b0b1339
skullcool <46402627+skullcool@users.noreply.github.com>
2019-03-09 13:33:13 +0300

    Create Ders20_Diziler_for_if.java

---
+++ netbeans/src/netbeans/Ders20_Diziler_for_if.java
@@ -0,0 +1,17 @@
+public class Ders20 {
+    
+    public static void main(String[] args) {
+      int[] rakamlar= {2,7,4,0,9,6,0};
+        for (int i = 0; i < rakamlar.length; i++) {
+            if (rakamlar[i]==0) {
+                System.out.println(i+"-var");
+            }else{
+                System.out.println(i+"-Yok");
+            }
+        }
+        
+        
+       
+    }
+    
+}
