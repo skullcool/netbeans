e11fc2497e0de02223f3b05668b5f84dd741f290
skullcool <46402627+skullcool@users.noreply.github.com>
2019-03-09 13:37:49 +0300

    Update Ders20_Diziler_for_if.java

--- netbeans/src/netbeans/Ders20_Diziler_for_if.java
+++ netbeans/src/netbeans/Ders20_Diziler_for_if.java
@@ -1,7 +1,8 @@
 public class Ders20 {
     
     public static void main(String[] args) {
-      int[] rakamlar= {2,7,4,0,9,6,0};
+      
+        int[] rakamlar= {2,7,4,0,9,6,0};
         for (int i = 0; i < rakamlar.length; i++) {
             if (rakamlar[i]==0) {
                 System.out.println(i+"-var");
@@ -9,7 +10,14 @@ public class Ders20 {
                 System.out.println(i+"-Yok");
             }
         }
-        
+        //----------------
+        String[] isimler={"Ali","Ayşe","Mehmet"};
+        for (int i = 0; i < isimler.length; i++) {
+            if (isimler[i].equals("Ali")) {
+                System.out.println(i+"-Var");
+            }
+ 
+        }
         
        
     }
